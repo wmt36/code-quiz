38c1059aa81c365caf9fcf21beb5a9f873d1625c
wmt36 <wmt36@Williams-iMac.local>
2020-07-08 17:28:00 -0600

    updated the homework to be a little bit more interactiver and functional

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # code-quiz
 This assignment has had me up on my toes as well as its taken over my dreams where i feel like i find the solution.
 
-i was able to get to the point where my start quiz timer works and already have my first question appered by minuplating the DOM with javascrip but im not able to actually retrieve my other question and choices when i clcik on my next button. will continue to play with it and upload the revised version when i am able to debug my problem.
+i was able to connect my next button to render the questions as well alow my start button to display the quiz as qwll as start the timer.,
--- sript.js
+++ sript.js
@@ -1,103 +1,109 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-const questions = document.getElementById("questions");
-const start = document.getElementById("start");
-const next = document.getElementById("next");
-const choiceA = document.getElementById("choiceA");
-const choiceB = document.getElementById("choiceB");
-const choiceC = document.getElementById("choiceC");
-const choiceD = document.getElementById("choiceD");
-const quiz = document.getElementById("quiz");
-const timer = document.getElementById("timer");
-const progress = document.getElementById("progress");
-
-
-
-let questionsList = [{
-    questions: "1. Commonly used data types DO NOT include:",
-    choiceA: "strings",
-    choiceB: "boolians", 
-    choiceC: "alerts", 
-    choiceD: "numbers",
-    correctAnswer: "B" 
-}, {
-    questions: "2. The Condiion in an if else statement is enclosed within?",
-    choiceA: "Quotes",
-    choiceB: "curly brackets",
-    choiceC: "parenthasees",
-    choiceD: "square brackets",
-    correctAnswer: "C"
-}, {
-    questions: "3. Arrays in JavaScript can be used to store?",
-    choiceA: "numbers and strings",
-    choiceB: "other arrays",
-    choiceC: "boolians", 
-    choiceD: "all of the above",
-    correctAnswer: "B"
-},{
-    questions: "4. A very useful tool used during development and debugging for printing content to the dubugger is?",
-    choiceA: "JavaScriopt",
-    choiceB: "terminal",
-    choiceC: "for loops",
-    choiceD: "console.log",
-    correctAnswer: "C"
-}];
+    const questions = document.getElementById("questions");
+    const start = document.getElementById("start");
+    const next = document.getElementById("next");
+    const choiceA = document.getElementById("choiceA");
+    const choiceB = document.getElementById("choiceB");
+    const choiceC = document.getElementById("choiceC");
+    const choiceD = document.getElementById("choiceD");
+    const quiz = document.getElementById("quiz");
+    const timer = document.getElementById("timer");
+    const progress = document.getElementById("progress");
 
 
 
-var counter = 60;
-var zero = 0;
-const lastQuestion = questionsList.length - 1;
-let runningQuestion = 0;
-runningQuestion;
-nextQuestion();
-renderQuestion();
+    let questionsList = [{
+        questions: "1. Commonly used data types DO NOT include:",
+        choiceA: "strings",
+        choiceB: "boolians",
+        choiceC: "alerts",
+        choiceD: "numbers",
+        correctAnswer: "B"
+    }, {
+        questions: "2. The Condiion in an if else statement is enclosed within?",
+        choiceA: "Quotes",
+        choiceB: "curly brackets",
+        choiceC: "parenthasees",
+        choiceD: "square brackets",
+        correctAnswer: "C"
+    }, {
+        questions: "3. Arrays in JavaScript can be used to store?",
+        choiceA: "numbers and strings",
+        choiceB: "other arrays",
+        choiceC: "boolians",
+        choiceD: "all of the above",
+        correctAnswer: "B"
+    }, {
+        questions: "4. A very useful tool used during development and debugging for printing content to the dubugger is?",
+        choiceA: "JavaScriopt",
+        choiceB: "terminal",
+        choiceC: "for loops",
+        choiceD: "console.log",
+        correctAnswer: "C"
+    }];
+
+
+
+    var counter = 60;
+    var zero = 0;
+    const lastQuestion = questionsList.length - 1;
+    let runningQuestion = 0;
+    //nextQuestion();
+    
 
 
 
 
-   
 
 
 
-$("#start").click(function(){
-     timer.innerHTML = counter;
-     var interval = setInterval(timeIt, 1000);
-function timeIt(){
-    counter--;
-    timer.innerHTML = counter;
-    if( counter == zero){
-        clearInterval(interval);
-        counter = 0;
+
+    $("#start").click(function () {
+        renderQuestion();
+        quiz.style.display = "block";
+        timer.innerHTML = counter;
+        var interval = setInterval(timeIt, 1000);
+        function timeIt() {
+            counter--;
+            timer.innerHTML = counter;
+            if (counter == zero) {
+                clearInterval(interval);
+                counter = 0;
+                
+            }
+        }
+
+
+    });
+    console.log(start);
+
+    $("#next").on("click", renderQuestion);
+    console.log(next);
+
+    function renderQuestion(e){
+       // e.preventDefault();
+        
+        let q = questionsList[runningQuestion];
+        questions.innerHTML = "<p>" + q.questions + "</p>";
+        choiceA.innerHTML = q.choiceA;
+        choiceB.innerHTML = q.choiceB;
+        choiceC.innerHTML = q.choiceC;
+        choiceD.innerHTML = q.choiceD;
+        runningQuestion++;
+        console.log("renderQuestion")
     }
-  } 
-   
-
-});
-console.log(start);
-
-$("#next").on("click", renderQuestion());
-console.log(next);
-
-function renderQuestion(){
-    let q = questionsList[runningQuestion];
-    questions.innerHTML = "<p>" + q.questions + "</p>";
-    choiceA.innerHTML = q.choiceA;
-    choiceB.innerHTML = q.choiceB;
-    choiceC.innerHTML = q.choiceC;
-    choiceD.innerHTML = q.choiceD;
-}
 
 
-function nextQuestion() {
-  for( i = 0; i < runningQuestion.length; i++)
-    renderQuestion();
-    }
+    // function nextQuestion() {
+    //     for (i = 0; i < runningQuestion.length; i++)
+    //         renderQuestion();
+    // }
 
-start.style.display = "block";
-renderQuestion();
-quiz.style.display = "block";
-renderProgress();
+    start.style.display = "block";
+
+    
+    //renderProgress();
 
 
 
